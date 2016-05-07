bc0bb9c80170d86c297be64ea431a048c4efaf8d
rjhdby <andrewgrom@rambler.ru>
2016-05-07 16:05:14 +0300

    2

--- request.php
+++ request.php
@@ -1,56 +1,58 @@
 <?php
-ini_set('display_errors', 'On');
-ini_set('display_startup_errors', 'On');
-header('Content-Type: text/html; charset=utf-8');
-if (isset ($_GET ['method'])) {
+ini_set ( 'display_errors', 'On' );
+ini_set ( 'display_startup_errors', 'On' );
+header ( 'Content-Type: text/html; charset=utf-8' );
+if ( isset( $_GET[ 'm' ] ) ) {
+    $_GET [ 'method' ] = $_GET [ 'm' ];
+}
+if ( isset ( $_GET [ 'method' ] ) ) {
     $_POST = $_GET;
 }
 
-function __autoload($class_name)
-{
+function __autoload ( $class_name ) {
     require_once 'class/' . $class_name . '.class.php';
 }
 
-switch (mb_strtolower($_POST ['method'])) {
+switch ( mb_strtolower ( $_POST [ 'method' ] ) ) {
     case 'getrole':
-        $response = new User($_POST);
-        $response->readRole();
+        $response = new User( $_POST );
+        $response->readRole ();
         break;
     case 'setrole':
-        $response = new User($_POST);
-        $response->setRole();
+        $response = new User( $_POST );
+        $response->setRole ();
         break;
     case 'getpolice':
         $response = new GetPolice();
-        $response->getList();
+        $response->getList ();
         break;
     case 'getobjects':
         $response = new GetObjects();
-        $response->getObjects();
+        $response->getObjects ();
         break;
     case 'create':
-        $response = new CreatePoint($_POST);
+        $response = new CreatePoint( $_POST );
         break;
     case 'cpk':
-        $response = new Point($_POST);
-        $response->changeKarma();
+        $response = new Point( $_POST );
+        $response->changeKarma ();
         break;
     case 'createyandexpoint':
-        $response = new CreateYandexPoint($_POST);
+        $response = new CreateYandexPoint( $_POST );
         break;
     default :
-        $response = new WrongMethod($_POST);
+        $response = new WrongMethod( $_POST );
 }
-if ($response->isError()) {
-    $result = array(
-        'ERROR' => array(
-            'text' => $response->getErrorText(),
-            'object' => $response->getErrorObject()
+if ( $response->isError () ) {
+    $result = array (
+        'ERROR' => array (
+            'text' => $response->getErrorText (),
+            'object' => $response->getErrorObject ()
         )
     );
 } else {
-    $result = array('RESULT' => $response->getResult());
+    $result = array ( 'RESULT' => $response->getResult () );
 }
-print_r(json_encode($result));
+print_r ( json_encode ( $result ) );
 //var_dump($result);
 ?>
